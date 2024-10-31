3dad46515705058a16f84c1585ea2b16dea437fa
MetallicSky <metlaski.corporatioN@gmail.com>
2024-10-31 10:59:43 +0300

    Добавьте файлы проекта.

---
+++ Concat Machine/Concat Machine.cpp
@@ -0,0 +1,26 @@
+﻿#include "Concat Machine.h"
+
+using std::cout;
+using std::endl;
+
+#ifdef PR_DEBUG
+#define LOG(X) cout << X << endl;
+void initDebugArgs(int& argc, const char**& argv) {
+	// Simulate values for argc and argv in debug mode
+	static const char* debug_argv[] = { "ConcatMachine.exe", "C:/Test Folder" };
+	argc = 2;
+	argv = debug_argv;
+}
+#else
+#define LOG(X)
+#define initDebugArgs(argc, argv)
+#endif
+
+int main(int argc, char* argv[])
+{
+	initDebugArgs(argc, (const char**&)argv);
+	LOG(argc);
+	cout << argc << endl;
+	cout << "Hello CMake.\n";
+	return 0;
+}
